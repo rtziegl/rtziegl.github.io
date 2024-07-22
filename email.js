4d5eb82c2ab4629a28776858c7418b6f7211e4d0
Ryley Ziegler <73793678+rtziegl@users.noreply.github.com>
2024-07-22 12:03:32 -0400

    email redo

--- email.js
+++ email.js
@@ -1,27 +1,20 @@
-document.getElementById('contact-form').addEventListener('submit', function(event) {
-    event.preventDefault();
+// email.js
 
-    const email = document.getElementById('email').value;
-    const subject = document.getElementById('subject').value;
-    const message = document.getElementById('message').value;
+// Obfuscated email address
+var user = 'rz';
+var domain = 'ryleyziegler.com';
+var email = user + '@' + domain;
 
-    fetch('https://my-flask-mailer-4fc6e1c7adda.herokuapp.com/send_mail', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ email, subject, message }),
-    })
-    .then(response => response.json())
-    .then(data => {
-        if (data.message === "Email sent successfully") {
-            alert("Email sent successfully!");
-        } else {
-            alert("Failed to send email.");
-        }
-    })
-    .catch((error) => {
-        console.error('Error:', error);
-        alert("An error occurred. Please try again.");
-    });
-});
+// Create the mailto link
+var mailtoLink = 'mailto:' + email;
+
+// Function to create the email link and set it
+function createMailtoLink() {
+    var mailIcon = document.getElementById('toggleForm');
+    if (mailIcon) {
+        mailIcon.parentNode.href = mailtoLink;
+    }
+}
+
+// Call the function to set the mailto link on page load
+document.addEventListener('DOMContentLoaded', createMailtoLink);
